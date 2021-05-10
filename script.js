60fd9328fa2de732031d3a689f69560b088d8edb
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2021-05-10 02:59:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,32 @@
 console.log("testing");
 let newEmailButton = document.getElementById("newEmailButton");
-//let emailValue = document.getElementById("emailValue").value;
-let submit = document.getElementById("0submit");
 let detailEmailButton = document.getElementById("detailEmailButton");
-let emailArray = [];
 let i = 0;
+
+//add new input field
 newEmailButton.addEventListener("click", (e) => {
-  addEmail();
+  i++;
+  let txtNewInputBox = document.createElement("input");
+  txtNewInputBox.setAttribute("class", "mb-3 form-control");
+  txtNewInputBox.setAttribute("id", `${i}emailValue`);
+  document.getElementById("newEmail").appendChild(txtNewInputBox);
+  console.log(i);
+
   e.preventDefault();
 });
-//add email
-function addEmail() {
-  i++;
-  let newEmail = document.getElementById("newEmail");
-  newEmail.innerHTML += ` <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="emailValue${i}" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div> 
-  <button type="submit" class="btn btn-primary" id="${i}submit" onclick="submitFunc(event,this.id)" >Submit</button>
-  </form>
-  `;
-}
 
-function submitFunc(e, ind) {
-  console.log(i);
-  ind = ind.charAt(0);
-
-  let emailValue = document.getElementById(`emailValue${ind}`).value;
-
-  emailArray.push(emailValue);
-  console.log(emailArray);
-  clear(ind);
-  e.preventDefault();
-}
-
-//show function
+//show detail()
 detailEmailButton.addEventListener("click", (e) => {
   let tableBody = document.getElementById("tableBody");
   tableBody.innerHTML = null;
-  emailArray.forEach((element) => {
-    if (element != "") {
-      let html = `<tr>
-    <th scope="col">${element}</th>
-    </tr>`;
-      tableBody.innerHTML += html;
-    }
-  });
+  for (let j = 0; j <= i; j++) {
+    let value = document.getElementById(`${j}emailValue`).value;
+    console.log(value);
 
+    let html = `<tr>
+    <th scope="col">${value}</th>
+    </tr>`;
+    tableBody.innerHTML += html;
+  }
   e.preventDefault();
 });
-
-const clear = (ind) => {
-  let emailValue = document.getElementById(`emailValue${ind}`);
-  emailValue.value = " ";
-};
