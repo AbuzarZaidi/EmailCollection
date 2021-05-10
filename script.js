fe8b67717af87f9d8ac2068e8fd30b4e9731a481
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2021-05-10 03:13:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ let i = 0;
 newEmailButton.addEventListener("click", (e) => {
   i++;
   let txtNewInputBox = document.createElement("input");
+  txtNewInputBox.setAttribute("type", "email");
   txtNewInputBox.setAttribute("class", "mb-3 form-control");
   txtNewInputBox.setAttribute("id", `${i}emailValue`);
   document.getElementById("newEmail").appendChild(txtNewInputBox);
+  document.getElementById(`${i}emailValue`).focus()
   console.log(i);
 
   e.preventDefault();
@@ -22,11 +24,16 @@ detailEmailButton.addEventListener("click", (e) => {
   for (let j = 0; j <= i; j++) {
     let value = document.getElementById(`${j}emailValue`).value;
     console.log(value);
-
-    let html = `<tr>
-    <th scope="col">${value}</th>
-    </tr>`;
-    tableBody.innerHTML += html;
+if(value==""){
+  continue;
+}
+else{
+  let html = `<tr>
+  <th scope="col">${value}</th>
+  </tr>`;
+  tableBody.innerHTML += html;
+}
+   
   }
   e.preventDefault();
 });
