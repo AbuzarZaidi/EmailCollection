be485b955293c44caee8a02b135774e46d782064
AbuzarZaidi <abuzarzaidi947@gmail.com>
2021-05-09 16:58:28 -0800

    commit

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 console.log("testing");
 let newEmailButton = document.getElementById("newEmailButton");
-let emailValue = document.getElementById("emailValue").value;
-let submit = document.getElementById("submit");
+//let emailValue = document.getElementById("emailValue").value;
+let submit = document.getElementById("0submit");
 let detailEmailButton = document.getElementById("detailEmailButton");
 let emailArray = [];
+let i=0;
 newEmailButton.addEventListener("click", (e) => {
+ addEmail();
+e.preventDefault();
+});
+//add email
+function addEmail(){
+i++;
   let newEmail = document.getElementById("newEmail");
   newEmail.innerHTML += ` <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="emailValue" aria-describedby="emailHelp">
+    <input type="email" class="form-control" id="emailValue${i}" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div> 
-  <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+  <button type="submit" class="btn btn-primary" id="${i}submit" onclick="submitFunc(event,this.id)" >Submit</button>
   </form>
   `;
-e.preventDefault();
-});
-submit.addEventListener("click", (e) => {
-  let emailValue = document.getElementById("emailValue").value;
-  console.log(emailValue);
+}
+
+function submitFunc(e,ind) {
+ console.log(i);
+ind=ind.charAt(0)
+  let emailValue = document.getElementById(`emailValue${ind}`).value;
+ // console.log(emailValue);
   emailArray.push(emailValue);
   console.log(emailArray);
-  clear();
+ // clear();
   e.preventDefault();
-});
+}
 
 //show function
 detailEmailButton.addEventListener("click", (e) => {
-  clear();
+ // clear();
+ 
   let tableBody = document.getElementById("tableBody");
-
+  tableBody.innerHTML=null;
 emailArray.forEach((element)=>{
     let html = `<tr>
 <th scope="col">${element}</th>
@@ -42,8 +52,8 @@ emailArray.forEach((element)=>{
 });
 
 //clear function
-const clear = () => {
-  let emailValue = document.getElementById("emailValue");
-  emailValue.value = " ";
-  tableBody.innerHTML=null;
-};
+// const clear = () => {
+//   let emailValue = document.getElementById("emailValue");
+//   emailValue.value = " ";
+//   
+// };
